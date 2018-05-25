d857cf6cbeb52188330503d56e9f84cabc063e2f
bob chase <${SETUP_GIT_EMAIL:-bob.chase@gmail.com}>
2018-05-25 08:35:27 -0400

    hack to make python-mode work with vanilla mac

--- vimsetup.sh
+++ vimsetup.sh
@@ -7,7 +7,9 @@ mv vim-pathogen/autoload .
 rm -rf vim-pathogen
 mkdir bundle
 cd bundle
-git clone https://github.com/klen/python-mode.git
+git clone --recursive https://github.com/python-mode/python-mode
+cd python-mode
+git co e3f714b
 cd $HOME
 ln -s git/base-config/vimrc .vimrc
 mkdir .ropeproject
